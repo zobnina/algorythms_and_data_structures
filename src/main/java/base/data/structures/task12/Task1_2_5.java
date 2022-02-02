e01e0529f080417fd6ae48f750811a588124af88
Alexandra Zobnina <azobnina@neoflex.ru>
2022-02-02 14:52:12 +0300

    task 1_2_5

---
+++ src/main/java/base/data/structures/task12/Task1_2_5.java
@@ -0,0 +1,33 @@
+package base.data.structures.task12;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Stack;
+
+public class Task1_2_5 {
+
+    private static final BufferedReader READER = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+
+        final String sp = " ";
+        READER.readLine();
+        final String numbers = READER.readLine();
+        final int[] arr = Arrays.stream(numbers.split(sp)).mapToInt(Integer::parseInt).toArray();
+        final int sectionSize = Integer.parseInt(READER.readLine());
+        final StringBuilder stringBuilder = new StringBuilder(numbers.length());
+        for (int start = 0; start <= arr.length - sectionSize; start++) {
+            final Stack<Map.Entry<Integer, Integer>> queue = new Stack<>();
+            Arrays.stream(Arrays.copyOfRange(arr, start, start + sectionSize)).forEach(number -> {
+                int max = queue.isEmpty() || number > queue.peek().getValue() ? number : queue.peek().getValue();
+                queue.push(Map.entry(number, max));
+            });
+            stringBuilder.append(queue.peek().getValue());
+            stringBuilder.append(sp);
+        }
+        System.out.println(stringBuilder);
+    }
+}
